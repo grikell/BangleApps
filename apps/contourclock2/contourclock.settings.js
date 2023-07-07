33ac3f947a6f9223d30d5db26c2f6b4cd4398000
grikell <giorgio.richelli@gmail.com>
2023-07-07 15:06:05 +0200

    Update contourclock.settings.js

--- apps/contourclock2/contourclock.settings.js
+++ apps/contourclock2/contourclock.settings.js
@@ -1,6 +1,6 @@
 (function(back) {
   Bangle.setUI("");
-  var settings = require('Storage').readJSON('contourclock.json', true) || {};
+  var settings = require('Storage').readJSON('contourclock2.json', true) || {};
   if (settings.fontIndex==undefined) {
     settings.fontIndex=0;
     settings.widgets=true;
@@ -17,27 +17,27 @@
       "< Back" : () => back(),
       'Widgets': {
         value: (settings.widgets !== undefined ? settings.widgets : true),
-        onchange : v => {settings.widgets=v; require('Storage').writeJSON('contourclock.json', settings);}
+        onchange : v => {settings.widgets=v; require('Storage').writeJSON('contourclock2.json', settings);}
       },
       'Weekday': {
         value: (settings.weekday !== undefined ? settings.weekday : true),
-        onchange : v => {settings.weekday=v; require('Storage').writeJSON('contourclock.json', settings);}
+        onchange : v => {settings.weekday=v; require('Storage').writeJSON('contourclock2.json', settings);}
       },
       'Date': {
         value: (settings.date !== undefined ? settings.date : true),
-        onchange : v => {settings.date=v; require('Storage').writeJSON('contourclock.json', settings);}
+        onchange : v => {settings.date=v; require('Storage').writeJSON('contourclock2.json', settings);}
       },
       'Hide widgets, weekday and date when locked': {
         value: (settings.hideWhenLocked !== undefined ? settings.hideWhenLocked : false),
-        onchange : v => {settings.hideWhenLocked=v; require('Storage').writeJSON('contourclock.json', settings);}
+        onchange : v => {settings.hideWhenLocked=v; require('Storage').writeJSON('contourclock2.json', settings);}
       },
       'Tap to show': {
         value: (settings.tapToShow !== undefined ? settings.tapToShow : false),
-        onchange : v => {settings.tapToShow=v; require('Storage').writeJSON('contourclock.json', settings);}
+        onchange : v => {settings.tapToShow=v; require('Storage').writeJSON('contourclock2.json', settings);}
       },
       'Twist to show': {
         value: (settings.twistToShow !== undefined ? settings.twistToShow : false),
-        onchange : v => {settings.twistToShow=v; require('Storage').writeJSON('contourclock.json', settings);}
+        onchange : v => {settings.twistToShow=v; require('Storage').writeJSON('contourclock2.json', settings);}
       },
       'set Font': () => fontMenu() 
     });
@@ -46,21 +46,21 @@
     Bangle.setUI("");
     savedIndex=settings.fontIndex;
     saveListener = setWatch(function() {          //save changes and return to settings menu
-      require('Storage').writeJSON('contourclock.json', settings);
+      require('Storage').writeJSON('contourclock2.json', settings);
       Bangle.removeAllListeners('swipe');
       Bangle.removeAllListeners('lock');
       mainMenu();
     }, BTN, { repeat:false, edge:'falling' });
     lockListener = Bangle.on('lock', function () { //discard changes and return to clock
       settings.fontIndex=savedIndex;
-      require('Storage').writeJSON('contourclock.json', settings);
+      require('Storage').writeJSON('contourclock2.json', settings);
       Bangle.removeAllListeners('swipe');
       Bangle.removeAllListeners('lock');
       mainMenu();
     });
     swipeListener = Bangle.on('swipe', function (direction) {
-      var { fontName, digits  } = require('contourclock').getDigits(settings.fontIndex+direction);
-      require('contourclock').drawClock(fontName, digits);
+      var { fontName, digits  } = require('contourclock2').getDigits(settings.fontIndex+direction);
+      require('contourclock2').drawClock(fontName, digits);
       if (fontName) {
         settings.fontIndex+=direction;
         g.clearRect(0,g.getHeight()-36,g.getWidth()-1,g.getHeight()-36+16);
@@ -72,7 +72,7 @@
     g.reset();
     g.clearRect(0,24,g.getWidth()-1,g.getHeight()-1);
     g.setFont('6x8:2x2').setFontAlign(0,-1);
-    var { fontName, digits  } = require('contourclock').getDigits(settings.fontIndex);
+    var { fontName, digits  } = require('contourclock2').getDigits(settings.fontIndex);
     g.drawString(fontName,g.getWidth()/2,g.getHeight()-36);
     g.drawString('Button to save',g.getWidth()/2,g.getHeight()-18);
   }

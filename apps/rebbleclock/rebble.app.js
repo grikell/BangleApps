be75e706d19d402588bc0c54044b1904aa30a55b
Giorgio <giorgio.richelli@gmail.com>
2024-04-21 19:36:00 +0200

    bugFix

--- apps/rebbleclock/rebble.app.js
+++ apps/rebbleclock/rebble.app.js
@@ -303,8 +303,7 @@ Graphics.prototype.setFontKdamThmor = function(scale) {
     g.clearRect(x + 2, y + 2 + 2, x + wi - 4 - 2, y + 2 + hi - 2); // centre
     g.setColor(g.theme.fg);
     g.fillRect(x + wi - 3, y + 2 + (((hi - 1) / 2) - 1), x + wi - 2, y + 2 + (((hi - 1) / 2) - 1) + 4); // contact
-//    g.fillRect(x + 3, y + 5, x + 4 + avgBattery() * (wi - 12) / 100, y + hi - 1); // the level
-    g.fillRect(x + 3, y + 5, x + 4 + meanBattery() * (wi - 12) / 100, y + hi - 1); // the level
+    g.fillRect(x + 3, y + 5, x + 4 + avgBattery() * (wi - 12) / 100, y + hi - 1); // the level
 
     if (Bangle.isCharging()) {
       g.setBgColor(settings.bg);
